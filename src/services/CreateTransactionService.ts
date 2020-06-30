3e181239975469d79fce1281b9dc55aa097aa9f0
Igor Ramon <igor-ramon@outlook.com>
2020-06-30 16:52:18 -0300

    Desafio concluido

---
+++ src/services/CreateTransactionService.ts
@@ -0,0 +1,37 @@
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import Transaction from '../models/Transaction';
+
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
+class CreateTransactionService {
+  private transactionsRepository: TransactionsRepository;
+
+  constructor(transactionsRepository: TransactionsRepository) {
+    this.transactionsRepository = transactionsRepository;
+  }
+
+  public execute({ title, value, type }: Request): Transaction {
+    if (!['income', 'outcome'].includes(type)) {
+      throw new Error('Transaction not is valid');
+    }
+    const { total } = this.transactionsRepository.getBalance();
+
+    if (type === 'outcome' && value > total) {
+      throw new Error('You cannot overcome your limit');
+    }
+
+    const appointment = this.transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+
+    return appointment;
+  }
+}
+
+export default CreateTransactionService;
